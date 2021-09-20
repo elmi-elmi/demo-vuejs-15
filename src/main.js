f38227fbf202578ec8bcbacd8ef1723a92785fac
elmi <shahrokhelmi@gmail.com>
2021-09-20 17:01:50 +0430

    video 7

--- src/main.js
+++ src/main.js
@@ -13,6 +13,9 @@ const store = createStore({
     mutations: {
         increment(state) {
             state.counter = state.counter + 1
+        },
+        increase(state, payLoad) {
+            state.counter = state.counter + payLoad.value;
         }
     }
 });
