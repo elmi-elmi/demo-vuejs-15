d3e272187debb17d4ca810ca25496dbdb66e8698
elmi <shahrokhelmi@gmail.com>
2021-09-20 16:30:11 +0430

    video 5

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import { createStore } from 'vuex';
 
 const app = createApp(App);
-
+const store = createStore({
+    state() {
+        return {
+            counter: 1
+        }
+    }
+});
+app.use(store);
 app.mount('#app');
